fc2ea31c5948c62e12d881ad5e877510d834870a
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2024-04-20 21:20:48 +0530

    add gitignore

--- ecom/settings.py
+++ ecom/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'crispy_forms',
+    # 'bootstrap4',
+
 
 ]
 
@@ -76,25 +78,26 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecom',
-        'USER': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'PASSWORD': 'ashu2468@$^*',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ecom',
+#         'USER': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'PASSWORD': 'ashu2468@$^*',
+#
+#     }
+# }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Password validation
@@ -143,5 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static/image')
 
 MEDIA_URL = "/images/"
 MEDIA_ROOT = BASE_DIR
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
